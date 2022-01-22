e5672b7a0eb358bc8d0ed5d130706267306eb13b
seankwarren <64291729+seankwarren@users.noreply.github.com>
2022-01-22 04:12:05 -0500

    Add files via upload

--- Plot.js
+++ Plot.js
@@ -1,17 +1,5 @@
 class Plot {
-    constructor(
-        x,
-        y,
-        size =              {width:300,height:200},
-        axisWeight =        1,
-        borderWeight =      1,
-        dataWeight =        1,
-        yBound =            {min:-0.5,max:3.5},
-        xBound =            {min:-0.5,max:5.5},
-        axisColor =    "black",
-        borderColor =  "black",
-        backgroundColor = "white",
-        dataColor =    "black"
+    constructor(x,y,size,axisWeight,borderWeight,dataWeight,yBound,xBound,axisColor,borderColor,backgroundColor,dataColor
     ) {
         this.xData = x;
         this.yData = y;
@@ -27,60 +15,70 @@ class Plot {
         this.dataColor = dataColor;
         this.xBound = xBound;
         this.yBound = yBound;
-
-        let canvas = document.createElement("CANVAS");
-        this.canvas = canvas;
-        this.ctx = canvas.getContext("2d");
+        this.axisX;
+        this.axisY;
         this.graph_HTML = "";
+  
 
-        // get pixel info
+        // assign other properties (xData_px,yData_px,axisX,axisY)
         this.getAxisCoords();
         this.getXinPixel();
         this.getYinPixel();
 
         // draw
+        this.draw();
+    }
+
+    
+    draw() {
         this.addBorder2SVG();
         this.addAxes2SVG();
         this.addData2SVG();
         this.finalizeSVG();
     }
 
+    // fetch the pixel locations for the axis
     getAxisCoords() {
         this.axisX = widthToPx(0,this.xBound.min,this.xBound.max,this.size.width);
         this.axisY = heightToPx(0,this.yBound.min,this.yBound.max,this.size.height);
-    };
+    }
 
+    // convert x data values to their pixel values based on the defined bounds
     getXinPixel() {
         for (var i=0;i<this.xData.length;i++){
             this.xData_px[i] = widthToPx(this.xData[i],this.xBound.min,this.xBound.max,this.size.width);
         }
     }
 
+    // converts y data values to their pixel values based on the defined bounds
     getYinPixel() {
         for (var i=0;i<this.yData.length;i++){
             this.yData_px[i] = heightToPx(this.yData[i],this.yBound.min,this.yBound.max,this.size.height);
         }
     }
 
-    // draw border and axis
+    // add border to svg
     addBorder2SVG() {
         this.graph_HTML += 
         `<rect width="${this.size.width}" height="${this.size.height}" fill="${this.borderColor}"/>
         <rect x="${this.borderWeight}" y="${this.borderWeight}" width="${this.size.width-2*this.borderWeight}" height="${this.size.height-2*this.borderWeight}" fill="${this.backgroundColor}"" />\n`;
     }
 
+    // add axes to svg
     addAxes2SVG() {
         this.graph_HTML +=
         `<line x1="${this.axisX}" y1="0" x2="${this.axisX}" y2="${this.size.height}" style="stroke:${this.axisColor};stroke-width:${this.axisWeight}"/>
         <line x1="0" y1="${this.axisY}" x2="${this.size.width}" y2="${this.axisY}" style="stroke:${this.axisColor};stroke-width:${this.axisWeight}" />\n`;
     }
 
+    // add data to svg
     addData2SVG() {
         for (var i=1;i<this.xData.length;i++) {
             this.graph_HTML +=  `<line x1="${this.xData_px[i-1]}" y1="${this.yData_px[i-1]}" x2="${this.xData_px[i]}" y2="${this.yData_px[i]}" style="stroke:${this.dataColor};stroke-width:${this.dataWeight}" />\n`
         }
     }
 
+    // format and append SVG to HTML
     finalizeSVG() {
         var img = document.createElement("svg");
         var base = document.getElementById("base");
